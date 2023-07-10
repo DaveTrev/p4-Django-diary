fa70d04a3b25e0210ab10a14f02b242e1483d193
DaveTrev <trevaskisdavid@gmail.com>
2023-07-10 16:01:21 +0000

    UPdate allowed hosts value in settings.py

--- p4_django_diary/settings.py
+++ p4_django_diary/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["cpddiary-bd5599156530.herokuapp.com/", "8000-davetrev-p4djangodiary-8rojtdy8ldg.ws-eu101.gitpod.io"]
+ALLOWED_HOSTS = ["cpddiary-bd5599156530.herokuapp.com", "8000-davetrev-p4djangodiary-8rojtdy8ldg.ws-eu101.gitpod.io"]
 
 
 # Application definition
@@ -133,15 +133,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+MEDIA_URL = '/media/'
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 STATIC_URL = '/static/'
-
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-MEDIA_URL = '/media/'
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
