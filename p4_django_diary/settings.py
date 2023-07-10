f6a0ad3cbf8e03685b8f61b832cd4f6bb8a1c74a
DaveTrev <trevaskisdavid@gmail.com>
2023-07-10 15:22:38 +0000

    added os environ secret key

--- p4_django_diary/settings.py
+++ p4_django_diary/settings.py
@@ -27,7 +27,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u#r&y1cwsm55(vf9+klmu3z*2@t5tpn!b8*xr3c6n($#8s1_fp'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
