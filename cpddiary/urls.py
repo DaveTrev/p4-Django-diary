e53d5507748916d3e1eee6f4f13fde9e62108f7a
DaveTrev <trevaskisdavid@gmail.com>
2023-07-30 15:24:44 +0000

    added diary entry detail url & view

--- cpddiary/urls.py
+++ cpddiary/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import DiaryList
+from .views import DiaryList, DiaryDetail
 
 urlpatterns = [
-    path('', DiaryList.as_view(), name="diary-entry"),
+    path('', DiaryList.as_view(), name="cpdlist"),
+    path('diary_entry/<int:pk>/', DiaryDetail.as_view(), name='cpdlist'),
 ]
--- cpddiary/views.py
+++ cpddiary/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from .models import diary_entry
 # Create your views here.
 
 
 class DiaryList(ListView):
+    model = diary_entry
+    context_object_name = 'entry'
+
+
+class DiaryDetail(DetailView):
     model = diary_entry
