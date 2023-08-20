e753cc1f33b355f69267c7fc2eb0ed5bb88e2d01
DaveTrev <trevaskisdavid@gmail.com>
2023-08-20 15:15:16 +0000

    added stylesheet as tempory template

--- p4_django_diary/settings.py
+++ p4_django_diary/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 
 MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
