88a8e7d47df17b4be4ee1a5ed0905d379eef30fb
DaveTrev <trevaskisdavid@gmail.com>
2023-09-10 13:08:21 +0000

    fixed log cpd link on index page jumbotron

--- p4_django_diary/settings.py
+++ p4_django_diary/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+from django.contrib.messages import constants as messages
 import dj_database_url
 
 if os.path.isfile("env.py"):
@@ -61,6 +62,14 @@ SITE_ID = 1
 LOGIN = 'accounts/login'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+MESSAGE_TAGS = {
+        messages.DEBUG: 'alert-info',
+        messages.INFO: 'alert-info',
+        messages.SUCCESS: 'alert-success',
+        messages.WARNING: 'alert-warning',
+        messages.ERROR: 'alert-danger',
+    }
 
 
 MIDDLEWARE = [
